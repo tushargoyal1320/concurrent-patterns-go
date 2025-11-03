0948b6f0d428ac81d96b4bfeaa98e8a2402a7e5b
Tushar Goyal <goyaltushar1320@gmail.com>
2025-11-03 22:18:07 +0530

    Fan-out

--- fan-in-fan-out/main.go
+++ fan-in-fan-out/main.go
@@ -38,6 +38,56 @@ func main() {
 
 }
 
+func main2() {
+
+	ch1, err := read("file1.csv")
+	if err != nil {
+		fmt.Print("Err : ", err)
+		return
+	}
+
+	br1 := breakup("1", ch1)
+	br2 := breakup("2", ch1)
+	br3 := breakup("3", ch1)
+
+	for {
+		if br1 == nil && br2 == nil && br3 == nil {
+			break
+		}
+
+		select {
+		case _, ok := <-br1:
+			if !ok {
+				br1 = nil
+			}
+		case _, ok := <-br2:
+			if !ok {
+				br2 = nil
+			}
+		case _, ok := <-br3:
+			if !ok {
+				br3 = nil
+			}
+		}
+	}
+	fmt.Println("All completed, exiting ....")
+}
+
+func breakup(worker string, ch <-chan []string) chan struct{} {
+
+	che := make(chan struct{})
+
+	go func() {
+		for v := range ch {
+			fmt.Println(worker, v)
+		}
+		close(che)
+	}()
+
+	return che
+
+}
+
 func merge1(chns ...<-chan []string) <-chan []string {
 
 	var wg sync.WaitGroup
